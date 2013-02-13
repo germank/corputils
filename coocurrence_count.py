ff2fb30eafe86cb51a068edfab10012b72ac374c
German Kruszewski <german.kruszewski@unitn.it>
2013-02-13 23:37:47 +0100

    saving everything as sparse

--- coocurrence_count.py
+++ coocurrence_count.py
@@ -7,10 +7,11 @@ import sys
 import pickle
 import sqlite3
 import numpy as np
+from datetime import datetime
 from threading import Lock, Thread, Condition
 
 MANY = 100000
-saving_thread = None
+
 
 def main():
     parser = argparse.ArgumentParser(description=
@@ -31,6 +32,7 @@ def main():
     #TODO: add option to customize dense or sparse
 
     args = parser.parse_args()
+    sys.stderr.write("Started at {0}\n".format(str(datetime.now())))
     per_output_db = os.path.join(args.output_dir, 'peripheral.db')
     core_output_db = os.path.join(args.output_dir, 'core.db')
     #make sure outdir exists
@@ -39,20 +41,26 @@ def main():
     except OSError:
         pass
 
-    with open(args.cols) as f_cols:
-        cols = [col.rstrip('\n') for col in f_cols]
-    with open(args.rows) as f_rows:
-        rows = [row.rstrip('\n') for row in f_rows]
+    if args.cols:
+        with open(args.cols) as f_cols:
+            cols = [col.rstrip('\n') for col in f_cols]
+    else:
+        cols = None
+    if args.rows:
+        with open(args.rows) as f_rows:
+            rows = [row.rstrip('\n') for row in f_rows]
+    else:
+        rows = None
 
     col2id = dict((col,i) for i,col in enumerate(cols))
     row2id = dict((row,i) for i,row in enumerate(rows))
-    coocurrences = {}
-    peripheral_coocurrences = {}
-    peripheral_coocurrences_lock = Lock()
-    core_coocurrences = {}
-    core_coocurrences_lock = Lock()
+    #coocurrences = {}
+
     
     i=0
+    core = SparseCounter(core_output_db)
+    per = SparseCounter(per_output_db)
+
     for l in fileinput.input(args.input):
         i+=1
         if i % 1000 == 0:
@@ -60,101 +68,101 @@ def main():
             if i % 80000 == 0:
                 sys.stderr.write('\n')
         [w1,marker,w2] = l.rstrip('\n').split('\t')
-        #counts core space occurrences
-        #if marker not in coocurrences:
-        #    coocurrences[marker] = np.zeros((len(rows),len(cols)),dtype=np.int)
-        #marker_coocurrences = coocurrences[marker]
-        #if w1 in row2id and w2 in col2id:
-        #    marker_coocurrences[(row2id[w1],col2id[w2])] += 1
-        #counts peripheral space occurrences
-        if w1 in row2id and w2 in col2id:
-            save_sparse(core_coocurrences_lock, 
-                        core_coocurrences, w1, marker, w2)
-            ask_writing_if_many(core_coocurrences_lock,
-                                core_coocurrences, 
-                                core_output_db)
-        if args.compose_op in w1 and w1.split(args.compose_op)[1] in row2id:
-            save_sparse(peripheral_coocurrences_lock, 
-                        peripheral_coocurrences, w1, marker, w2)
-            ask_writing_if_many(peripheral_coocurrences_lock,
-                                peripheral_coocurrences, 
-                                per_output_db)
+        if args.compose_op in w1:
+            tg = w1.split(args.compose_op)[1]
+            if (not row2id or tg in row2id) and (not col2id or w2 in col2id):
+                per.count(w1, marker, w2)
+        else:
+            if (not row2id or w1 in row2id) and (not col2id or w2 in col2id):
+                core.count(w1, marker, w2)
+
     sys.stderr.write('\n')
-    for marker,marker_coocurrences in coocurrences.iteritems():
-        sys.stderr.write('writing {0}.pkl...\t'.format(marker))
-        save_dense_matrix(marker_coocurrences,
-            args.output_dir, marker + '.pkl')
-        sys.stderr.write('done\n')
+
     #wait for any pending saves
-    with peripheral_coocurrences_lock:
-        if saving_thread:
-            sys.stderr.write('wating for write in sparse matrix...\t')
-            saving_thread.join()
-            sys.stderr.write('done\n')
-    #no more threads now, but there can be some residual
-    if len(peripheral_coocurrences) > 0:
-        sys.stderr.write('saving peripheral matrix...\t')
-        save_sparse_matrix(peripheral_coocurrences_lock,
-                           peripheral_coocurrences, per_output_db)
-        sys.stderr.write('done\n')
-    if len(peripheral_coocurrences) > 0:
+    core.join()
+    per.join()
+    #save residuals
+    if len(core)>0:
         sys.stderr.write('saving core matrix...\t')
-        save_sparse_matrix(core_coocurrences_lock,
-                           core_coocurrences, core_output_db)
+        core.save()
         sys.stderr.write('done\n')
+    if len(per)>0:
+        sys.stderr.write('saving peripheral matrix...\t')
+        per.save()
+        sys.stderr.write('done\n')
+    sys.stderr.write("Finished at {0}\n".format(str(datetime.now())))
         
-def save_sparse(peripheral_coocurrences_lock, 
-                peripheral_coocurrences, w1, marker, w2):
-    with peripheral_coocurrences_lock:
-        marker_peripheral_coocurrences = \
-                peripheral_coocurrences.setdefault(marker, dict())
-        if (w1,w2) not in marker_peripheral_coocurrences:
-            marker_peripheral_coocurrences[(w1,w2)] = 0
-        marker_peripheral_coocurrences[(w1,w2)] += 1
-
-
-def ask_writing_if_many(sparse_coocurrences_lock,
-                        sparse_coocurrences, output_db):
-    with sparse_coocurrences_lock:
-        if len(sparse_coocurrences) < MANY:
-            return
-        if not saving_thread:
-            saving_thread = Thread(target=save_sparse_matrix, 
-                                   args=[sparse_coocurrences_lock,
-                                     sparse_coocurrences, output_db])
-            saving_thread.start()
+        
+class SparseCounter():
+    def __init__(self, output_db):
+        self.coocurrences = {}
+        self.coocurrences_lock = Lock()
+        self.saving_thread = None
+        self.saving_thread_lock = Lock()
+        self.output_db = output_db
     
-def save_sparse_matrix(sparse_coocurrences_lock, 
-                             sparse_coocurrences, output_db):
-    con = sqlite3.connect(output_db, isolation_level='EXCLUSIVE')
-    con.execute('BEGIN EXCLUSIVE')
-    #database locked, let's lock the sparse_coocurrences
-    with sparse_coocurrences_lock:
-        for marker in sparse_coocurrences.keys():
-            marker_sparse_coocurrences = sparse_coocurrences[marker]
-            marker_table = '{0}'.format(marker)
-            con.execute("CREATE TABLE IF NOT EXISTS {0}(pivot text, "
-                        "context text, occurrences int, PRIMARY "
-                        "KEY(pivot,context))".format(marker_table))
-            for(w1,w2),c in marker_sparse_coocurrences.iteritems():
-                query = "INSERT OR REPLACE INTO {0} VALUES('{1}','{2}', "
-                "coalesce((select occurrences from {0} WHERE pivot ='{1}' AND "
-                "context='{2}'),0) + {3})".format(
-                        marker_table, w1.replace("'", "''"), 
-                        w2.replace("'", "''"), c)
-                try:
-                    con.execute(query)
-                except sqlite3.OperationalError:
-                    print query
-                    raise
-            #clear from memory
-            del sparse_coocurrences[marker]
-        con.commit()
-        con.close()
-        #going away
-        #FIXME: shouldn't be the task of this function
-        saving_thread = None
+    def count(self, w1, marker, w2):
+        with self.coocurrences_lock:
+            if marker not in self.coocurrences:
+                self.coocurrences[marker] = {}
+            marker_coocurrences = self.coocurrences[marker]
+            if (w1,w2) not in marker_coocurrences:
+                marker_coocurrences[(w1,w2)] = 0
+            marker_coocurrences[(w1,w2)] += 1
+            self.check_dump()
     
+    def __len__(self):
+        return sum([len(mc) for mc in self.coocurrences.itervalues()])
+    
+    def check_dump(self):
+        with self.saving_thread_lock:
+            if len(self) >= MANY\
+            and not self.saving_thread:
+                self.saving_thread = Thread(target=self.run_dump, args=[self])
+                self.saving_thread.start()
+                
+    def run_dump(self):
+        '''main for the saving thread'''
+        self.save()
+        with self.saving_thread_lock:
+            self.saving_thread=None
+    
+    def join(self):
+        '''IMPORTANT: should be called before exiting to ensure that
+        there is no pending write'''
+        with self.saving_thread_lock:
+            if self.saving_thread:
+                sys.stderr.write('wating for write in sparse matrix...\t')
+                self.saving_thread.join()
+                sys.stderr.write('done\n')
+    
+    def save(self):
+        con = sqlite3.connect(self.output_db, isolation_level='EXCLUSIVE')
+        con.execute('BEGIN EXCLUSIVE')
+        #database locked, let's lock the sparse_coocurrences
+        with self.coocurrences_lock:
+            for marker in self.coocurrences.keys():
+                marker_sparse_coocurrences = self.coocurrences[marker]
+                marker_table = '{0}'.format(marker)
+                con.execute("CREATE TABLE IF NOT EXISTS {0}(pivot text, "
+                            "context text, occurrences int, PRIMARY "
+                            "KEY(pivot,context))".format(marker_table))
+                for(w1,w2),c in marker_sparse_coocurrences.iteritems():
+                    query = ("INSERT OR REPLACE INTO {0} VALUES('{1}','{2}', "
+                    "coalesce((select occurrences from {0} WHERE pivot ='{1}' AND "
+                    "context='{2}'),0) + {3})").format(
+                            marker_table, w1.replace("'", "''"), 
+                            w2.replace("'", "''"), c)
+                    try:
+                        con.execute(query)
+                    except sqlite3.OperationalError:
+                        print query
+                        raise
+                #clear from memory
+                del self.coocurrences[marker]
+            con.commit()
+            con.close()
+
     
 def save_dense_matrix(m, outdir, fname):
     outfile = os.path.join(outdir, fname)
